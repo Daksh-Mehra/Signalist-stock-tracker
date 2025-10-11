cd0e3c81413067fe38b35e0904b78eecf7c28957
Daksh-Mehra <daksh.m2003bv@gmail.com>
2025-10-11 16:22:48 +0530

    improve css

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -24,7 +24,7 @@ const UserDropdown = () => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+                <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback className="text-yellow-900 bg-yellow-500 font-bold text-sm ">{user.name[0]}</AvatarFallback>
